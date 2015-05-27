46ead20b73ff981d0f18a3eef8bb8d75c86ace5e
병국 <ppusarida@gmail.com>
2015-05-28 00:30:36 +0900

    awt 기본 틀

---
+++ src/MouseHooking.java
@@ -0,0 +1,14 @@
+import java.awt.Frame;
+
+
+public class MouseHooking {
+	
+	public static void main(String[] args) {
+		
+		Frame frame = new Frame("Mouse hooking");
+		frame.setSize(300,300);
+		frame.setVisible(true);
+	}
+	
+
+}
